338e3e09b0887c685c76a149ac8c9eb230ce1ff6
ElectricCatHub <80710915+ElectricCatHub@users.noreply.github.com>
2021-06-03 16:45:16 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>My favourite cats</h1>
+    <ul>
+      <li>Fluffy cats</li>
+      <li>Naughty cats</li>
+      <li>Cute cats</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
